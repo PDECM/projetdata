62752385dc4ad1051932a95d4c201df51dff4934
PDECM <152015957+PDECM@users.noreply.github.com>
2024-04-06 16:35:55 +0200

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -280,7 +280,7 @@ def prerun(criteres_choisis,testing_offre,Top_actu_base):
     table_finale_export = table_finale_export.reset_index(drop = True)
     data_finale = table_finale_export.dropna(subset = ['lieuTravail.latitude', 'lieuTravail.longitude'], inplace=True)
 
-    return result_df
+    return train_df
 
 def run():
     
