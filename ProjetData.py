6b08be3eddb94152b2e3a05abd4596b26464c399
PDECM <152015957+PDECM@users.noreply.github.com>
2024-04-06 16:11:12 +0200

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -329,7 +329,7 @@ def run():
       correspondance_table = pd.read_excel('Correspondance_coderome.xlsx')
       rome_code = correspondance_table.loc[correspondance_table.RomeLib == secteur, 'Codes'].iloc[0] 
       criteres_choisis.append('valnumcoderome')
-      testing_offre.append(secteur)
+      testing_offre.append(rome_code)
 
       if code_NAF != "" :
         criteres_choisis.append('ValNumCodeNaf')
